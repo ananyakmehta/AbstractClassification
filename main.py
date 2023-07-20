47dc3d49f99d3d04fb76addb540e5eb47e89ecc2
Ananya Mehta <ananya.k.mehta@gmail.com>
2023-07-20 22:35:13 +0530

    Add plumbing for model evaluation

--- main.py
+++ main.py
@@ -1,14 +1,48 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
 import framework.davinci as davinci
 import framework.gpt_3p5 as gpt_3p5
+import base64
+from io import BytesIO
+from matplotlib.figure import Figure
+import numpy as np
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'df0331cefc6c2b9a5d0208a726a5d1c0fd37324feba25506'
-output = []
 
-@app.route('/')
-def index():
-    return render_template('result.html', messages=output)
+def plot_model_accuracy():
+    # Generate the figure **without using pyplot**.
+    fig = Figure(figsize=(10,5))
+    ax = fig.add_subplot(projection='3d')
+
+    colors = ['r', 'g', 'b', 'y']
+    yticks = [3, 2, 1, 0]
+    for c, k in zip(colors, yticks):
+        # Generate the random data for the y=k 'layer'.
+        xs = np.arange(20)
+        ys = np.random.rand(20)
+
+        # You can provide either a single color or an array with the same length as
+        # xs and ys. To demonstrate this, we color the first bar of each set cyan.
+        cs = [c] * len(xs)
+        cs[0] = 'c'
+
+        # Plot the bar graph given by xs and ys on the plane y=k with 80% opacity.
+        ax.bar(xs, ys, zs=k, zdir='y', color=cs, alpha=0.8)
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+    # On the y-axis let's only label the discrete values that we have data for.
+    ax.set_yticks(yticks)
+
+    # Save it to a temporary buffer.
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
+
+    # Embed the result in the html output.
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    return data
 
 def map_model_output(model_dict):
     display = [
@@ -45,9 +79,12 @@ def map_model_output(model_dict):
         display[i]['content'] = model_dict[display[i]['title']]
     return display
 
+@app.route('/')
+def index():
+    return redirect(url_for('classify'))
+
 @app.route('/classify/', methods=('GET', 'POST'))
 def classify():
-    global output
     if request.method == 'POST':
         title = request.form['title']
         abstract = request.form['content']
@@ -68,10 +105,25 @@ def classify():
             elif model == "Model B":
                 model_dict = davinci.model_davinci(title, abstract, temperature)
                 output = map_model_output(model_dict)
+            return render_template('result_classify.html', messages=output)
 
-            return redirect(url_for('index'))
+    return render_template('input_classify.html')
 
-    return render_template('input.html')
+@app.route('/evaluate/', methods=('GET', 'POST'))
+def evaluate():
+    if request.method == 'POST':
+        mintemp = float(request.form.get('mintemp'))
+        print(mintemp)
+        maxtemp = float(request.form.get('maxtemp'))
+        print(maxtemp)
+        for checkbox in 'model1', 'model2':
+            value = request.form.get(checkbox)
+            print(value)
+
+        data = plot_model_accuracy()
+        return render_template('result_evaluate.html', messages=data)
+
+    return render_template('input_evaluate.html')
 
 if __name__ == "__main__":
     # This is used when running locally only. When deploying to Google App
